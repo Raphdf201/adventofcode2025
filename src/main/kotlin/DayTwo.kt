1d6da4c262aaf1bc1945e948d9970f41a0d39e3a
Raphdf201 <raphdf201@hotmail.com>
2025-12-02 20:52:13 -0500

    d2p2

--- src/main/kotlin/DayTwo.kt
+++ src/main/kotlin/DayTwo.kt
@@ -17,5 +17,17 @@ fun dayTwoPartOne(input: String): ULong {
 }
 
 fun dayTwoPartTwo(input: String): ULong {
-    return 0uL
+    val ranges = input.split(",")
+    var total = 0uL
+
+    ranges.forEach { range ->
+        val edges = range.split("-")
+        for (num in edges[0].toULong()..edges[1].toULong()) {
+            if (Regex("(\\d+)\\1+").matches(num.toString())) {
+                total += num
+            }
+        }
+    }
+
+    return total
 }
