687bbfd1517ef0c6eb50c491b4921099e1e9f1b8
Raphdf201 <raphdf201@hotmail.com>
2025-12-04 21:00:31 -0500

    continue day 4

--- src/main/kotlin/DayFour.kt
+++ src/main/kotlin/DayFour.kt
@@ -1,13 +1,27 @@
 package net.raphdf201.adventofcode2025
 
-const val empty = '.'
-const val paper = '@'
-
 fun dayFourPartOne(input: List<String>): Int {
     var count = 0
 
-    for (row in input.indices) for (col in input[row].indices) {
-        if (hasMoreThan4Papers(input, row, col)) count++
+    for (row in input.indices) {
+        for (col in input[row].indices) {
+            if (input[row][col] != '@') {
+                var papers = 0
+
+                for (rowOffset in -1..1) {
+                    for (colOffset in -1..1) {
+                        val innerRow = row + rowOffset
+                        val innerCol = col + colOffset
+
+                        if (innerRow in input.indices && innerCol in input[innerRow].indices) {
+                            if (input[innerRow][innerCol] == '@') papers++
+                        }
+                    }
+                }
+
+                if (papers < 4) count++
+            }
+        }
     }
 
     return count
@@ -15,19 +29,4 @@ fun dayFourPartOne(input: List<String>): Int {
 
 fun dayFourPartTwo(input: List<String>): Int {
     return 0
-}
-
-fun hasMoreThan4Papers(input: List<String>, i1: Int, i2: Int): Boolean {
-    if (input[i1][i2] == paper) return false
-
-    var count = 0
-
-    for (rowOffset in -1..1) for (colOffset in -1..1) {
-        val row = i1 + rowOffset
-        val col = i2 + colOffset
-
-        if (row in input.indices && col in input[row].indices) if (input[row][col] == paper) count++
-    }
-
-    return count >= 4
 }
