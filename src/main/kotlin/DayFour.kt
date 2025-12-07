32a8fa07404ce6873ef1a020fa9129ded08c529b
Raphdf201 <raphdf201@hotmail.com>
2025-12-07 18:29:35 -0500

    d4p2

--- src/main/kotlin/DayFour.kt
+++ src/main/kotlin/DayFour.kt
@@ -27,5 +27,34 @@ fun dayFourPartOne(input: List<String>): Int {
 }
 
 fun dayFourPartTwo(input: List<String>): Int {
-    return 0
+    return removeRolls(input.map { it.toCharArray() })
+}
+
+private fun removeRolls(input: List<CharArray>): Int {
+    var removedThisRound = 0
+
+    for (row in input.indices) {
+        for (col in input.indices) {
+            if (input[row][col] != '@') continue
+            var papers = 0
+
+            for (rowOffset in -1..1) {
+                for (colOffset in -1..1) {
+                    val innerRow = row + rowOffset
+                    val innerCol = col + colOffset
+                    if (innerRow in input.indices && innerCol in input[innerRow].indices) {
+                        if (input[innerRow][innerCol] == '@') papers++
+                    }
+                }
+            }
+
+            if (papers < 5) {
+                removedThisRound++
+                input[row][col] = '.'
+            }
+        }
+    }
+
+    if (removedThisRound == 0) return 0
+    return removedThisRound + removeRolls(input)
 }
