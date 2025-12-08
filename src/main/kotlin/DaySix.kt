ef76bbef5ae74fb77451dd39dbe286f88a85f21f
Raphdf201 <raphdf201@hotmail.com>
2025-12-07 19:39:48 -0500

    d6p1

--- src/main/kotlin/DaySix.kt
+++ src/main/kotlin/DaySix.kt
@@ -1,7 +1,26 @@
 package net.raphdf201.adventofcode2025
 
-fun daySixPartOne(input: List<String>): UInt {
-    return 0u
+fun daySixPartOne(input: List<String>): ULong {
+    var total = 0uL
+    val list = mutableListOf<MutableList<ULong>>()
+
+    input.forEach {
+        for ((i, num) in it.split(Regex("\\s+")).withIndex()) {
+            if (num != "+" && num != "*") {
+                if (list.size <= i) list.add(mutableListOf())
+                if (num == "") continue
+                list[i].add(num.toULong())
+            } else {
+                total += if (num == "+") {
+                    list[i].sum()
+                } else {
+                    list[i].reduce { acc, value -> acc * value }
+                }
+            }
+        }
+    }
+
+    return total
 }
 
 fun daySixPartTwo(input: List<String>): UInt {
