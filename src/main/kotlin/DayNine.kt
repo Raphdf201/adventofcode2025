e9e17e00f1f3648ae5a9c9cf5185aba759b3648c
Raphdf201 <deschenest545@gmail.com>
2025-12-15 17:37:55 -0500

    d9p1

--- src/main/kotlin/DayNine.kt
+++ src/main/kotlin/DayNine.kt
@@ -1,18 +1,21 @@
 package net.raphdf201.adventofcode2025
 
-fun dayNinePartOne(input: List<Pair<Int, Int>>): Int {
-    var total = 0
+import kotlin.math.abs
+import kotlin.math.max
+
+fun dayNinePartOne(input: List<Pair<Int, Int>>): ULong {
+    var total = 0UL
 
     for (i in input.indices) {
         for (j in i + 1 until input.size) {
             val (x1, y1) = input[i]
             val (x2, y2) = input[j]
 
-            val width = kotlin.math.abs(x2 - x1) + 1
-            val height = kotlin.math.abs(y2 - y1) + 1
+            val width = abs(x2 - x1).toULong() + 1UL
+            val height = abs(y2 - y1).toULong() + 1UL
             val area = width * height
 
-            total = kotlin.math.max(total, area)
+            total = max(total, area)
         }
     }
 
